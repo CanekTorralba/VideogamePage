961933c6f851d0e6e697cecd432a6d2b5f7f4b88
CanekTorralba <canek.torralba@gmail.com>
2023-03-25 18:49:09 -0700

    Finish Landing page front and add Review page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'review',
+    loadChildren: () => import('./review/review.module').then( m => m.ReviewPageModule)
+  },
 ];
 
 @NgModule({
