c59f8ab315ca0287d07ec6ff5314120a825e9e53
CanekTorralba <canek.torralba@gmail.com>
2023-03-26 21:03:14 -0700

    Create Review Page

--- src/app/review/review.page.ts
+++ src/app/review/review.page.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-review',
   templateUrl: './review.page.html',
   styleUrls: ['./review.page.scss'],
 })
-export class ReviewPage implements OnInit {
+export class ReviewPage {
+  teamId: string = '';
+  score: number = 0;
+  comment: string = '';
+  reviews: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private storage: Storage) {
+    this.storage.create(); // call the create() method
   }
 
+  ionViewDidEnter() {
+    this.storage.get('reviews').then((reviews) => {
+      console.log('Saved reviews:', reviews);
+      if (reviews) {
+        this.reviews = JSON.parse(reviews);
+      }
+    });
+  }
+
+  saveReview() {
+    console.log('Saving review');
+    const review = {
+      teamId: this.teamId,
+      score: this.score,
+      comment: this.comment || '' // make comment an empty string if it's undefined
+    };
+
+    this.storage.get('reviews').then((reviews) => {
+      let savedReviews = [];
+
+      if (reviews) {
+        savedReviews = JSON.parse(reviews);
+      }
+
+      savedReviews.push(review);
+      console.log('Saved reviews:', savedReviews);
+      this.storage.set('reviews', JSON.stringify(savedReviews)).then(() => {
+        this.reviews.push(review);
+        this.teamId = '';
+        this.score = 0;
+        this.comment = '';
+      });
+    });
+  }
 }
